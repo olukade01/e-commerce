7506f46d27900655ee4d25e32ab61882c20ac649
Olukade Muzzammil <olukademuzzammil@gmail.com>
2022-10-10 09:46:58 +0100

    feat:

--- sanity_ecommerce/schemas/schema.js
+++ sanity_ecommerce/schemas/schema.js
@@ -3,8 +3,20 @@ import schemaTypes from 'all:part:@sanity/base/schema-type';
 import product from './product';
 import banner from './banner';
 import user from './user';
+import order from './order';
+import orderItem from './orderItem';
+import shippingAddress from './shippingAddress';
+import paymentResult from './paymentResult';
 
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([product, banner, user]),
+  types: schemaTypes.concat([
+    product,
+    banner,
+    user,
+    order,
+    orderItem,
+    shippingAddress,
+    paymentResult,
+  ]),
 });
