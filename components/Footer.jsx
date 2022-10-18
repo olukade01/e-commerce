9ad65105a91bb38d7bbaf886ebc736daf661c523
Olukade Muzzammil <olukademuzzammil@gmail.com>
2022-10-18 23:33:43 +0100

    fix: light mode and link fixed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,17 @@
-import React from "react";
-import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
+import Link from 'next/link';
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
       <p>2022 MUZ Store All rights reserved</p>
       <p className="icons">
-        <AiFillInstagram /> <AiOutlineTwitter />
+        <Link href="https://instagram.com/sir_olukade">
+          <AiFillInstagram />
+        </Link>
+        <Link href="https://twitter.com/olukadeM">
+          <AiOutlineTwitter />
+        </Link>
       </p>
     </div>
   );
