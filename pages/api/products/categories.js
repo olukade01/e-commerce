8aa56c5576342bd61362de47495c08ce8a28358e
Olukade Muzzammil <olukademuzzammil@gmail.com>
2022-10-19 16:31:51 +0100

    feat: added products

--- pages/api/products/categories.js
+++ pages/api/products/categories.js
@@ -3,7 +3,18 @@ import nc from 'next-connect';
 const handler = nc();
 
 handler.get(async (req, res) => {
-  const categories = ['Shirts', 'Speakers', 'Headphones', 'Wrist Watch'];
+  const categories = [
+    'Shirts',
+    'Speakers',
+    'Headphones',
+    'Wrist Watch',
+    'Gaming',
+    'Camera',
+    'Shoes',
+    'Gadgets',
+    'Shades',
+    'Bags',
+  ];
   res.send(categories);
 });
 
